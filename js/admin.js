56178475631223daf3252f5fab0d1cbd0725be2b
enveloperchou <enveloperchou@gmail.com>
2016-12-13 10:05:38 +0800

    first request

--- js/admin.js
+++ js/admin.js
@@ -2,10 +2,18 @@ var angular = require("angular");
 require("wangeditor");
 
 var app = angular.module('blog');
-app.controller('AdminController', function($scope){
+app.controller('AdminController', function($scope, $http){
 	var editor = new wangEditor('editor');
 	editor.create();
 	$scope.publish = function(){
-		alert("publish");
+		var content = "content=" + editor.$txt.html() + "&title=" + $scope.title;	
+		$http({
+			method:'POST',
+			url:'http://106.75.76.72:9000/publish',
+			data: content,
+			headers: {'Content-Type':'application/x-www-form-urlencoded'}
+		}).success(function(rep){
+				alert(rep);
+			});
 	};
 })
